bf74df11551c88f940c67d64cb4f42c0c0a79c9e
Jack Su <s2510156@ed.ac.uk>
2025-02-12 20:23:36 +0000

    fix routing

--- unigpt-frontend/next.config.ts
+++ unigpt-frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Remove if using SSR
+  trailingSlash: true, // Optional: Match Vercel routing
 };
 
 export default nextConfig;
